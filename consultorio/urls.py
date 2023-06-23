e341c51693c2277e962542e09e4b1924a7c1a686
o0leslyth40o <102860687+o0leslyth40o@users.noreply.github.com>
2023-06-23 19:59:30 -0400

    agregar vistas

--- consultorio/urls.py
+++ consultorio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
+from .import views
 
 
 URLPatterns=[
-
+    path('', views.index, name="index"),
 ]
--- consultorio/views.py
+++ consultorio/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'consultorio/index.html')
